e0c49b4aa4e19bb0edcb92a60c816c91b275862f
pekepekee <takkummm78@gmail.com>
2021-08-26 17:00:47 +0900

    add EnemyBullet

---
+++ shooting/Assets/pekepeke/EnemyBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    [SerializeField] float EBspeed = 5;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.forward * EBspeed;
+    }
+}
---
+++ shooting/Assets/pekepeke/EnemyMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] GameObject Ebullet;
+    private float k;
+    public Quaternion rotation = Quaternion.identity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("BulletSet");
+        k = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator BulletSet()
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            rotation.eulerAngles = new Vector3(0, k, 0);
+            yield return new WaitForSeconds(0.3f);
+            Instantiate(Ebullet, transform.position, rotation);
+            k += 10;
+        }
+    }
+}
