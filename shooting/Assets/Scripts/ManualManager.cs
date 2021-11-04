ecc7146f6571a54b0b42a302013c8b65b546a2b8
pekepekee <takkummm78@gmail.com>
2021-11-04 23:13:51 +0900

    sousahouhounogamennnotuika
    操作方法の追加

---
+++ shooting/Assets/Scripts/ManualManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualManager : MonoBehaviour
+{
+    public GameObject Background;
+    AudioSource AS;
+    public AudioClip choice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnclickManual()
+    {
+        AS.PlayOneShot(choice);
+        Background.SetActive(true);
+    }
+
+    public void OnclickBack()
+    {
+        AS.PlayOneShot(choice);
+        Background.SetActive(false);
+    }
+
+}
